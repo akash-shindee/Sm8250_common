500a8d6203852613b8f9a0b01cdf52ebd120876f
AAMIRR_ALI <amirwide@gmail.com>
2023-09-15 19:43:05 +0530

    sm8250-common: Do not dexpreopt prebuilt packages

--- common.mk
+++ common.mk
@@ -23,6 +23,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Inherit proprietary libraries
 $(call inherit-product, vendor/realme/sm8250-common/sm8250-common-vendor.mk)
 
+# Dex/ART optimization
+DONT_DEXPREOPT_PREBUILTS := true
+
 # ANT
 PRODUCT_PACKAGES += \
     com.dsi.ant@1.0.vendor
